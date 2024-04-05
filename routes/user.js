c87eb3194715e734102fb79eb5a67d852170f742
sazingakuulie <hanumantdoke143@gmail.com>
2024-04-06 00:00:15 +0530

    verify otp

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,7 @@ router.post('/reset-password', validatePassword, validate, isValidPassResetToken
 router.post("/sign-in", signInValidator, validate, signIn);
 router.get("/is-auth", isAuth, (req, res) => {
     const { user } = req;
-    res.json({ user: { id: user._id, name: user.name, email: user.email } });
+    res.json({ user: { id: user._id, name: user.name, email: user.email, isVerified: user.isVerified } });
 });
 
 module.exports = router;
